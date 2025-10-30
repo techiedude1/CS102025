1bc54ca97a48a896d9e32423d7b44a38b9e4f8be
techiedude1 <182171327+techiedude1@users.noreply.github.com>
2025-10-30 12:51:29 -0500

    Add Svelte plugin and configure base path
    Added Svelte plugin and set base path for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    plugins: [svelte()],
+  base: '/CS102025/', // 👈 Add this line
     const env = loadEnv(mode, '.', '');
     return {
       server: {
